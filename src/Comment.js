f4eda72393a06c814026b8442190941dc93bd8e1
kristopherbullinger <kris.bullinger@gmx.com>
2018-11-17 19:08:14 -0500

    Lab Complete

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,11 @@
-// add Comment component here
+import React, { Component } from 'react'
+
+export default class Comment extends Component {
+  render () {
+    const commentText = this.props.commentText
+    return (
+      <div className="comment">{commentText}</div>
+    )
+  }
+
+}
